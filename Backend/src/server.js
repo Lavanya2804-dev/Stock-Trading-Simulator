c90fc1217c65a4bdaa803c1ef435cc6f8e57c939
Lavanya2804-dev <lavanyasaniboeena@gmail.com>
2026-05-31 14:02:57 +0530

    Fix CORS for Vercel

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -24,7 +24,9 @@ const server = http.createServer(app);
 // SOCKET.IO
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+       "https://stock-trading-simulator-lake.vercel.app",
+    ],
     credentials: true,
   },
 });
